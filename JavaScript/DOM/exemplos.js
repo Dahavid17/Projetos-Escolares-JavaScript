7c656be9642136cdb07333070b70bc5ba1fcec76
Dahavid17 <davi.a.santos25@aluno.senai.br>
2026-02-25 15:30:07 -0300

    Examples PEND

---
+++ JavaScript/DOM/exemplos.js
@@ -0,0 +1,5 @@
+document.getElementById("conteudo").innerHTML = "<p>Adicionar conteudo html em codigo JS</p>";
+document.getElementById("mensagem").textContent = "textContent";
+document.getElementById("foto").setAttribute("src", "https://cursinhoparaetec.com.br/wp-content/uploads/2024/03/SENAI_Sao_Paulo_logo.png");
+let url = document.getElementById("link").getAttribute("href");
+console.log(url);
